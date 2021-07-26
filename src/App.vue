196b4e1f0c2b1713136599e5f6c6304ba4fcbd59
dajamante <aimaiga2@gmail.com>
2021-07-26 14:50:22 +0200

    some eating array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Rust logo" src="./assets/groundhog_bike.png" size=200/>
- <HelloWorld msg="A biking Sensor app" />
+  <p>
+ {{dataValues}}
+ </p>
  <MyChart />
 </template>
 
@@ -8,18 +10,34 @@
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import MyChart from './components/MyChart.vue'
+import { invoke } from '@tauri-apps/api/tauri'
+import { emit, listen } from '@tauri-apps/api/event'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
     MyChart,
+  },
+  data(){
+      return {
+          msg: "",
+          dataValues: [0,0,0,0,0],
+      };
+  },
+  async mounted(){
+    await invoke('init_process');
+    listen("distance_emitter", x => {
+        this.dataValues.push(Number((x as any).payload as string));
+        if(this.dataValues.length > 10) {
+          this.dataValues.shift();
+        } 
+    });
   }
-})
+});
 </script>
 
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
